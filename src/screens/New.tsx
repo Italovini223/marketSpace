67bd78561cc308c8ebe99ea51ede3f5062f682db
Italovini223 <italovinicius2018@gmail.com>
2023-07-30 14:17:46 -0300

    ✨ feat: finish New screen style

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -11,15 +11,18 @@ import { ProductImageCard } from '@components/ProductImageCard';
 import { SelectProductImage } from '@components/SelectProductImage';
 import { Input } from '@components/Input';
 import { HeadingTopic } from '@components/HeadingTopic';
+import { Button } from '@components/Button';
 
 export function New(){
   const [images, setImages] = useState(['https://s3-alpha-sig.figma.com/img/2b9d/0e16/12432335ff951fd0930c4fa7d271a8d5?Expires=1691366400&Signature=XiPh9ALBOtSoIsd3D87YAJc9lVScs9GK7zsTerKW0sRfKNTAaG0hoXD0ul88HcO2et9vHTX938Q9tc-Ak1x5~hxWt2H2g2mmdwluJ0dQwjBElqZdrK5W4Mzvt8IQp8~sW6j-7pet5AIcBjG~wDTPmKYCMgVRSn4fHSjSTFtm8JH0PtBWI8GMFX-Up51ObugYGPE1dVu2YCt9Lxfrh2M7IuQVRMvwiU2vJtp8ZBqmueIyn907R0pQQ5iaFoxw1mervzHdWHn0FN89uHol0~9cohcYbXPBuwsyFWy7adFYUro1o5jpdVK-fATr3G8JxoA7lorKy7CtiG~0CLnOjTX0Iw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4', 'https://s3-alpha-sig.figma.com/img/56a0/7e43/8d8a171dd0517c35b31d54312b139654?Expires=1691366400&Signature=cbYpGh8yRvzMUoCZKppUHnXYUCFbgZHoYka6OBzhlK3QJ50yPM66G-RLNWcsCIz6jr7huunlKIgifCfp14m5c71NiYxb~uUUYA5cqoqK4oYY7caAnKTydXAP-YPZfDHJyVO1KphgPE6cNIsaenkQSBwyAkgbkvnc7KhtPHFK2evDv0JehYmBtb64DZ2X17IDcglH1-laZD6Q0hbpHG8cadMl~ZVau3XxtzDdG3xBYrQBFXz7EIJEVlTVThCfQwiKkPbB6aGMy6oPPiP9Zc8VSOwBE0McnQI7zOBzY7TlSbXnbd57nnK2s8kpopEOtHm2dkSrpcBP8ntQE3R87Ko9tA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'])
+ const [paymentOptions, setPaymentOptions] = useState([])
   const { sizes, colors } = useTheme()
   const navigation = useNavigation<AppNavigatorRoutesProps>()
 
   function handleBack(){
     navigation.navigate('home')
   }
+
   return(
     <ScrollView flex={1} bg="gray.200" showsVerticalScrollIndicator={false}>
       <VStack flex={1} pb={Platform.OS === 'android' ? 'auto' : 2}>
@@ -122,17 +125,30 @@ export function New(){
           </VStack>
           <VStack mt={4}>
             <HeadingTopic title='Meios de pagamento aceitos'/>
-            <Checkbox.Group v>
+            <Checkbox.Group onChange={setPaymentOptions} value={paymentOptions}>
               <Checkbox value='boleto' mb={3}>Boleto</Checkbox>
               <Checkbox value='pix' mb={3}>Pix</Checkbox>
               <Checkbox value='dinheiro' mb={3}>Dinheiro</Checkbox>
-              <Checkbox value='credit' mb={3}>Cartão de Crédito</Checkbox>
-              <Checkbox value='deposit' mb={3}>Depósito Bancário</Checkbox>
+              <Checkbox value='credito' mb={3}>Cartão de Crédito</Checkbox>
+              <Checkbox value='deposito' mb={3}>Depósito Bancário</Checkbox>
             </Checkbox.Group>
           </VStack>
         </VStack>
         
       </VStack>
+      <HStack w="full" h={24} bg="gray.100" px={6} alignItems="center" >
+        
+          <Button 
+            title='Cancelar'
+            w="50%"
+            mr={3}
+          />
+          <Button 
+            title='Avançar'
+            variant="ghost"
+            w="50%"
+          />
+      </HStack>
     </ScrollView>
   )
 }
